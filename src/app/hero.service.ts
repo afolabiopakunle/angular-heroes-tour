0bb605e6891b2375c3aa389e28b5b750305f5091
Afolabi Opakunle <opakunleafolabi@gmail.com>
2021-12-07 00:04:30 +0100

    error handling implemented

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,23 +3,41 @@ import {HEROES} from "./mock-heroes";
 import {Hero} from "./hero";
 import {Observable, of} from "rxjs";
 import {MessageService} from "./message.service";
+import {HttpClient} from "@angular/common/http";
+import {catchError} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';
+
+  constructor(private messageService: MessageService,
+              private http: HttpClient) { }
 
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
   }
 
   getHero(id: number) {
     const hero = HEROES.find(hero => hero.id === id);
     this.messageService.add('HeroService: fetched hero id = ' + id);
     return of(hero);
+  }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T)
+    }
   }
 }
